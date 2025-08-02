a78ec1fd33a96120213665f8ab74f31e5dc43d5b
DeviantNinja <73482554+DeviantNinja@users.noreply.github.com>
2025-08-02 16:07:38 +0100

    Simple number guessing game. not really much to say about it. feels a little underwelming given that hte first project i tried was tic-tac-toe

---
+++ src/NumberGuessingGame.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+
+        int number = rand.nextInt(101);
+        int guessedNumber;
+        int guessCount = 0;
+        boolean gameOver = false;
+
+        while(!gameOver) {
+            guessCount++;
+            System.out.print("Guess a number between 1 and 100: ");
+            while(!input.hasNextInt()) {
+                System.out.print("Invalid Entry, Guess Again!");
+                input.next();
+            }
+
+            guessedNumber = input.nextInt();
+            if(guessedNumber == number) {
+                System.out.println("Congratulations you guessed the number!");
+                System.out.printf("it took you %d tries.\n",guessCount);
+                gameOver = true;
+                break;
+            }
+            System.out.printf("Incorrect Number. Your guess was %s !\n", guessedNumber > number ? "to high" : "to low");
+        }
+    }
+}
